2bb6a088d96085ce9e2529cd32970d9c611bc669
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-09-06 14:41:47 +0300

    added gui and lib

---
+++ libs/geocoding/geocoding.h
@@ -0,0 +1,26 @@
+#ifndef GEOCODING_H
+#define GEOCODING_H
+
+#include <QObject>
+#include <QNetworkAccessManager>
+
+class Geocoding : public QObject {
+    Q_OBJECT
+
+private:
+    QNetworkAccessManager _manager;
+
+public:
+    explicit Geocoding(QObject *parent = nullptr);
+
+public slots:
+    void start(const QString &location);
+
+private slots:
+    void onReply(QNetworkReply *reply);
+
+signals:
+    void complete(const QString &errorString, double lon, double lat);
+};
+
+#endif
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include "settingsdialog.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,3 +14,10 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_settingsButton_clicked()
+{
+    SettingsDialog dialog(this);
+    dialog.exec();
+}
+
